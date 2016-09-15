09f83dc13b2ef00d1668bf204cbe75619ab0b276
avarabyeu <andrei.varabyeu@gmail.com>
2016-09-15 16:11:26 +0300

    initial commit

---
+++ src/main/java/com/epam/reportportal/commons/TikaContentTypeResolver.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 EPAM Systems
+ * 
+ * 
+ * This file is part of EPAM Report Portal.
+ * https://github.com/epam/ReportPortal
+ * 
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+package com.epam.reportportal.commons;
+
+import org.apache.tika.detect.Detector;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+class TikaContentTypeResolver implements ContentTypeResolver {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TikaContentTypeResolver.class);
+
+	@Override
+	public String detectContentType(byte[] data) {
+		try {
+			AutoDetectParser parser = new AutoDetectParser();
+			Detector detector = parser.getDetector();
+			MediaType mediaType = detector.detect(TikaInputStream.get(data), new Metadata());
+			return mediaType.toString();
+		} catch (IOException e) {
+			LOGGER.error("Cannot read data stream", e);
+			return MediaType.OCTET_STREAM.toString();
+		}
+
+	}
+
+}
---
+++ src/test/java/com/epam/reportportal/commons/ThumbnailatorImplTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 EPAM Systems
+ * 
+ * 
+ * This file is part of EPAM Report Portal.
+ * https://github.com/epam/ReportPortal
+ * 
+ * Report Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Report Portal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+ 
+package com.epam.reportportal.commons;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Set of tests for Thumbnailator
+ * 
+ * @author Andrei Varabyeu
+ * 
+ */
+public class ThumbnailatorImplTest {
+
+	private static final String TEST_FILE_NAME = "meh.jpg";
+
+	private static final int THUMBNAIL_WIDTH = 50;
+
+	private static final int THUMBNAIL_HEIGHT = 60;
+
+	/**
+	 * Validates sizes of thumbnail
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void checkThumbnailator() throws IOException {
+		Thumbnailator thumbnailator = new ThumbnailatorImpl();
+		InputStream is = thumbnailator.createThumbnail(this.getClass().getClassLoader().getResourceAsStream(TEST_FILE_NAME));
+
+		BufferedImage image = ImageIO.read(is);
+
+		Assert.assertEquals("Incorrect image height", THUMBNAIL_HEIGHT, image.getHeight());
+		Assert.assertEquals("Incorrect image width", THUMBNAIL_WIDTH, image.getWidth());
+
+	}
+}
