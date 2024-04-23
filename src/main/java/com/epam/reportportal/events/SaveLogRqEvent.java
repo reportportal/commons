bfc9e72640e9b26c0cbf46f25ccbe1f7719685a8
Pavel Bortnik <pbortnik1207@gmail.com>
2024-04-23 22:45:29 +0300

    EPMRPP-89997 || Add reporting events to add possibility to report from plugins

--- src/main/java/com/epam/reportportal/events/SaveLogRqEvent.java
+++ src/main/java/com/epam/reportportal/events/SaveLogRqEvent.java
@@ -1,17 +1,21 @@
 package com.epam.reportportal.events;
 
 import com.epam.ta.reportportal.ws.reporting.SaveLogRQ;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.web.multipart.MultipartFile;
 
+@Getter
 public class SaveLogRqEvent extends ApplicationEvent {
 
-  private SaveLogRQ saveLogRQ;
-  private MultipartFile file;
+  private final String projectName;
+  private final SaveLogRQ saveLogRQ;
+  private final MultipartFile file;
 
-  public SaveLogRqEvent(Object source, SaveLogRQ saveLogRQ,
-      MultipartFile file) {
+  public SaveLogRqEvent(Object source, String projectName,
+      SaveLogRQ saveLogRQ, MultipartFile file) {
     super(source);
+    this.projectName = projectName;
     this.saveLogRQ = saveLogRQ;
     this.file = file;
   }
