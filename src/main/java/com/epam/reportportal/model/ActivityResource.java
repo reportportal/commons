23fba8bf8f20776460cefd5c9fa095bc1ef71b6e
Ivan Kustau <86599591+IvanKustau@users.noreply.github.com>
2024-05-22 17:55:17 +0300

    EPMRPP-90366 || Add descriptions for ActivityResource (#24)

--- src/main/java/com/epam/reportportal/model/ActivityResource.java
+++ src/main/java/com/epam/reportportal/model/ActivityResource.java
@@ -42,46 +42,49 @@ public class ActivityResource {
 
   @NotNull
   @JsonProperty(value = "id", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The unique ID of the activity", example = "1")
   private Long id;
 
   @NotNull
   @JsonProperty(value = "user", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The user who performed the activity", example = "user")
   private String user;
 
   @NotNull
   @JsonProperty(value = "loggedObjectId", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The ID of the object on which activity was performed", example = "1")
   private Long loggedObjectId;
 
   @NotNull
   @JsonProperty(value = "lastModified", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The time the activity was last modified", example = "2021-07-01T12:00:00Z")
   private Instant lastModified;
 
   @NotNull
   @JsonProperty(value = "actionType", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The type of action performed", example = "startLaunch")
   private String actionType;
 
   @NotNull
   @JsonProperty(value = "objectType", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The type of object on which the activity was performed", example = "LAUNCH")
   private String objectType;
 
   @NotNull
   @JsonProperty(value = "projectId", required = true)
-  @Schema(requiredMode = RequiredMode.REQUIRED)
+  @Schema(requiredMode = RequiredMode.REQUIRED, description = "The ID of the project in which the activity was performed", example = "1")
   private Long projectId;
 
   @JsonProperty(value = "projectName")
+  @Schema(description = "The name of the project in which the activity was performed", example = "project")
   private String projectName;
 
   @JsonProperty(value = "details")
+  @Schema(description = "The details of the activity, for example history of value", example = "{\"history\": [{\"field\": \"status\", \"newValue\": \"FAILED\", \"oldValue\": \"PASSED\"}]}")
   private Object details;
 
   @JsonProperty(value = "objectName")
+  @Schema(description = "The name of the object on which the activity was performed", example = "Launch name")
   private String objectName;
 
 }
