4b4542856be1605ab85fa3b4306e286529819793
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-10-11 20:10:31 +0300

    EPMRPP-95887 remove generated models

--- src/main/java/com/epam/reportportal/api/model/PatchOperation.java
+++
@@ -1,9 +0,0 @@
-package com.epam.reportportal.api.model;
-
-
-import javax.annotation.Generated;
-
-
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.8.0")
-public interface PatchOperation {
-}
