604d763c9837b4f40553d94b6ad5084eaf24f771
siarhei_hrabko <siarhei_hrabko@epam.com>
2025-03-07 11:57:29 +0300

    EPMRPP-88765 JSON validation error message

--- src/main/java/com/epam/reportportal/rules/commons/exception/rest/RestExceptionHandler.java
+++ src/main/java/com/epam/reportportal/rules/commons/exception/rest/RestExceptionHandler.java
@@ -167,7 +167,7 @@ public class RestExceptionHandler extends DefaultHandlerExceptionResolver {
   protected ModelAndView handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
       HttpServletRequest request,
       HttpServletResponse response, Object handler) throws IOException {
-    return handleCustomException(request, response, new RuntimeException("Invalid JSON input"));
+    return handleCustomException(request, response, new HttpMessageNotReadableException("Invalid JSON input"));
   }
 
   @Override
